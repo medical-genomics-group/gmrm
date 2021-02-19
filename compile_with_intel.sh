1f63025ff31f4443a3b45b3f7c5e6d0d6cc107b2
orliac <etienne.orliac@unil.ch>
2021-02-19 23:49:41 +0100

    Adjusted to be callable from anywhere

--- compile_with_intel.sh
+++ compile_with_intel.sh
@@ -2,18 +2,17 @@
 
 set -e
 
+# before loading modules!!!
+export ARDYH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo ARDYH_ROOT = $ARDYH_ROOT
+[[ "$ARDYH_ROOT" == *ardyh ]] || (echo "ARDYH_ROOT ($ARDYH_ROOT) expected to end with \"ardyh\"" && exit 1)
+
 module purge
 module load intel intel-mpi boost
 module list
 
-export ARDYH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
-
 cd $ARDYH_ROOT
-
 ARDYH_EXE=ardyh_i
-
 EXEC=$ARDYH_EXE make -j8 $1 || exit 1
-
 export ARDYH_EXE=$ARDYH_EXE
-
 cd -
